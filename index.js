3df4f50ee330b41ae63cf47d23329a04012282e9
Aric Crosson Bouwers <aricbouwers@outlook.com>
2023-03-15 15:21:41 -0600

    adding dotenv setup, and adding .DS_Store to .gitignore

---
+++ index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env.MONGO_URL);
+
+async function main() {
+   await mongoose.connect();
+}
+
+main().catch((err) => console.log(err));
